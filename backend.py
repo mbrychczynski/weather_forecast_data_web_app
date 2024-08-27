eb94a4645e3680ecc5cd4346ac65762498dc8356
Mateusz Brychczynski <mat.brych@gmail.com>
2024-08-27 21:22:50 +0100

    Add Sky Conditions

--- backend.py
+++ backend.py
@@ -4,18 +4,14 @@ import os
 API_KEY = os.getenv("OPENWEATHERMAP_API_KEY")
 
 
-def get_data(place, forecast_days, option):
+def get_data(place, forecast_days):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     filtered_data = filtered_data[:8 * forecast_days]
-    if option == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if option == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Wloclawek", forecast_days=2, option="Sky"))
+    print(get_data(place="Wloclawek", forecast_days=2))
--- main.py
+++ main.py
@@ -8,7 +8,21 @@ days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the num
 option = st.selectbox("Select data to view: ", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} {'day' if days == 1 else 'days'} in {place}")
 
-dates, temperatures = get_data(place, days, option)
+if place:
+    filtered_data = get_data(place, days)
 
-figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+    if option == "Temperature":
+        temperatures = [dict["main"]["temp"] for dict in filtered_data]
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+        st.plotly_chart(figure)
+
+    if option == "Sky":
+        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+        images = {
+            "Clear": "images/clear.png",
+            "Clouds": "images/cloud.png",
+            "Rain": "images/rain.png",
+            "Snow": "images/snow.png"}
+        image_path = [images[condition] for condition in sky_conditions]
+        st.image(image_path, width=115)
