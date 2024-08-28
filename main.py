9f715bc57dd9d77c754aea904ce7cdf2bd8614f1
Mateusz Brychczynski <mat.brych@gmail.com>
2024-08-28 10:20:00 +0100

    Fixing temperature scale, and informing user if user put down not existing Town

--- main.py
+++ main.py
@@ -9,20 +9,23 @@ option = st.selectbox("Select data to view: ", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} {'day' if days == 1 else 'days'} in {place}")
 
 if place:
-    filtered_data = get_data(place, days)
+    try:
+        filtered_data = get_data(place, days)
 
-    if option == "Temperature":
-        temperatures = [dict["main"]["temp"] for dict in filtered_data]
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
-        st.plotly_chart(figure)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] - 272.15 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-    if option == "Sky":
-        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
-        images = {
-            "Clear": "images/clear.png",
-            "Clouds": "images/cloud.png",
-            "Rain": "images/rain.png",
-            "Snow": "images/snow.png"}
-        image_path = [images[condition] for condition in sky_conditions]
-        st.image(image_path, width=115)
+        if option == "Sky":
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            images = {
+                "Clear": "images/clear.png",
+                "Clouds": "images/cloud.png",
+                "Rain": "images/rain.png",
+                "Snow": "images/snow.png"}
+            image_path = [images[condition] for condition in sky_conditions]
+            st.image(image_path, width=115)
+    except KeyError:
+        st.warning("That place does not exist")
